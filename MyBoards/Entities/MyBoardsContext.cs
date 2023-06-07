d92d577b3ec8321266db2661fe56035fe62a1116
amalecki <amalecki@RK-38>
2023-06-07 14:59:32 +0200

    Add project files.

---
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+using System;
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options) 
+        {
+        }    
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User>  Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkItem>()
+                .Property(x => x.State)
+                .IsRequired();
+            modelBuilder.Entity<WorkItem>()
+                .Property(x => x.Area)
+                .HasColumnType("varchar(200)");
+            modelBuilder.Entity<WorkItem>(eb =>
+            {
+                eb.Property(wi => wi.IterationPath)
+            });
+        }
+
+    }
+}
---
+++ MyBoards/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using MyBoards.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyBoardsContext>(
+        option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyBoardsConnectionString"))
+    );
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
